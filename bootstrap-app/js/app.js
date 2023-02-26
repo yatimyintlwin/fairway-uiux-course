dd99f7e0f6de2caacc894542f161735371c69b95
Yati Myint Lwin <yatimyintlwin43@gmail.com>
2023-02-26 12:42:49 +0630

    [Added] Services & Portfolios Sections

--- bootstrap-app/js/app.js
+++ bootstrap-app/js/app.js
@@ -1,2 +1,25 @@
 // AOS JS
 AOS.init();
+
+// Owl Carousel JS
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: true,
+  navText: ["PREV", "NEXT"],
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+// Lightgallery JS
+lightGallery(document.getElementById("anchor-tag"));
